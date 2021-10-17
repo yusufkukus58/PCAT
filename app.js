a8bd0adf6828ec673e7e38cf701a647328b71b6f
yusuf kukus <ykukus10@gmail.com>
2021-10-18 00:12:41 +0300

    template engine eklendi

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const path = require("path");
+
+const ejs = require('ejs');
 const app = express();
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render('index');
+});
+app.get("/about", (req, res) => {
+  res.render('about');
+});
+app.get("/add", (req, res) => {
+  res.render('add');
 });
 const port = 3000;
 
