076509b013df526e27e8e8c224c252fd99b66268
yusuf kukus <ykukus10@gmail.com>
2021-10-17 23:07:18 +0300

    static dosyalar kaydedildi

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Merhaba");
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
 });
 const port = 3000;
 
