f65de624d39cf59a765c6118e9dd9f409d74692e
yusuf kukus <ykukus10@gmail.com>
2021-10-17 22:10:04 +0300

    nodemon eklendi

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const fn = function () {
-  console.log("dasda");
-};
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Merhaba");
+});
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldıe`);
+});
