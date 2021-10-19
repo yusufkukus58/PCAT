48afdb6fac06df751a7de18a1a553633188bb4fa
yusuf kukus <ykukus10@gmail.com>
2021-10-19 13:19:40 +0300

    photo ejs si eklendi

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const ejs = require('ejs');
-const path = require('path');
-const Photo = require('./models/Photo');
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const path = require("path");
+const Photo = require("./models/Photo");
+
+
 const app = express();
+
 //connect db
-mongoose.connect('mongodb://localhost/pcat-test-db', {
+mongoose.connect("mongodb://localhost/pcat-test-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-
+});
 
 app.set("view engine", "ejs");
 //MIDDLEWARES
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
-  res.render('index', {
-    photos
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", {
+    photos,
+  });
+});
+app.get("/photos/:id", async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render("photo", {
+    photo,
   });
 });
 app.get("/about", (req, res) => {
@@ -28,9 +36,10 @@ app.get("/about", (req, res) => {
 app.get("/add", (req, res) => {
   res.render("add");
 });
-app.post('/photos', async (req, res) => { // async - await yapısı kullanacğız.
-  await Photo.create(req.body)            // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
-  res.redirect('/')
+app.post("/photos", async (req, res) => {
+  // async - await yapısı kullanacğız.
+  await Photo.create(req.body); // body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
+  res.redirect("/");
 });
 const port = 3000;
 
--- models/Photo.js
+++ models/Photo.js
@@ -3,13 +3,13 @@ const Schema = mongoose.Schema;
 
 //create schema
 const PhotoSchema = new Schema({
-    title: String,
-    description: String,
-   image: String,
+  title: String,
+  description: String,
+  image: String,
   dateCreated: {
     type: Date,
     default: Date.now,
-  }
+  },
 });
 
 const Photo = mongoose.model('Photo', PhotoSchema);
